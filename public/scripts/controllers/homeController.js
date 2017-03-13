020cbf261bb525bf9f6ace55c5b7b405ffd6345e
John Arkema <john.arkema@gmail.com>
2017-03-13 17:10:53 -0500

    loggedIn

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,10 +1,4 @@
 myApp.controller('homeController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
     console.log('home Controller');
-    $scope.loggedIn = false;
-    $scope.loggedOut = true;
-
-    $scope.init = function() {
-        $scope.loggedIn = true;
-        $scope.loggedOut = false;
-    }
+    
 }]);
