3d448b98d08253be4b01714fcf3e63df38fbb488
John Arkema <john.arkema@gmail.com>
2017-03-20 18:05:19 -0500

    maps

--- public/scripts/controllers/mapsController.js
+++ public/scripts/controllers/mapsController.js
@@ -1,4 +1,17 @@
 myApp.controller('mapsController', ['$scope', '$http', function($scope, $http) {
     console.log('maps Controller');
-    
+
+
+    $scope.initMap = function() {
+        console.log('initMap');
+        var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+        var mapOptions = {
+            zoom: 7,
+            center: myLatlng,
+            mapTypeId: 'roadmap'
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+    }
+    // $scope.initMap();
 }]);
