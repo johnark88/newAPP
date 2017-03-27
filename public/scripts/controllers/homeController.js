6337604397f7a5c7c4ec949a07cd90e333e7a907
John Arkema <john.arkema@gmail.com>
2017-03-26 19:44:13 -0500

    comments

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,10 +1,14 @@
-myApp.controller('homeController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+myApp.controller('homeController', ['$scope', '$http', function($scope, $http, $interval) {
     console.log('home Controller');
+    //url to hit
     var getURL = 'http://www.reddit.com/r/churning/hot.json';
+
+    //global arrays 
     $scope.urlArray = [];
     $scope.titleArray = [];
 
     $scope.getReddit = function() {
+        //hit Reddit API
         $http({
             method: 'GET',
             url: getURL
@@ -12,12 +16,11 @@ myApp.controller('homeController', ['$scope', '$http', '$interval', function($sc
             console.log('response', response.data);
             $scope.a = response.data;
             $scope.churningHot = $scope.a.data.children;
-
+            //gets title and url from JSON object from http call
             angular.forEach($scope.churningHot, function(item) {
                 $scope.titleArray.push(item.data.title);
                 $scope.urlArray.push(item.data.url);
-            });
-        });
-
-    };
-}]);
+            }); //end for each
+        }); //end then
+    }; //end scope dot reddit
+}]); //end controller
