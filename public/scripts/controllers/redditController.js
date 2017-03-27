be1212ce79ff34fb38ac997ed6df5fcfabdb8b26
John Arkema <john.arkema@gmail.com>
2017-03-27 18:45:35 -0500

    style

--- public/scripts/controllers/redditController.js
+++ public/scripts/controllers/redditController.js
@@ -2,10 +2,13 @@ myApp.controller('redditController', ['$scope', '$http', function($scope, $http,
     console.log('reddit Controller');
     //url to hit
     var getURL = 'http://www.reddit.com/r/churning/hot.json';
+    var random = 'http://www.reddit.com/r/churning/random.json';
 
     //global arrays
     $scope.urlArray = [];
     $scope.titleArray = [];
+    $scope.randomArray = [];
+    $scope.c = [];
 
     $scope.getReddit = function() {
         //hit Reddit API
@@ -23,4 +26,16 @@ myApp.controller('redditController', ['$scope', '$http', function($scope, $http,
             }); //end for each
         }); //end then
     }; //end scope dot reddit
+
+
+    $scope.randomReddit = function(){
+      $http({
+        method: 'GET',
+        url: random
+      }).then(function(response){
+          console.log(response.data, 'res');
+          $scope.b = response.data;
+          console.log($scope.b, 'b');
+      });//end then
+    };//end scope random reddit
 }]); //end controller
