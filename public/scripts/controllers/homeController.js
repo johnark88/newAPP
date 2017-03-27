dcc9951dfcbc1adb14d47e273a0c422858fbee34
John Arkema <john.arkema@gmail.com>
2017-03-26 19:41:58 -0500

    reddit api

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,4 +1,23 @@
 myApp.controller('homeController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
     console.log('home Controller');
-    
+    var getURL = 'http://www.reddit.com/r/churning/hot.json';
+    $scope.urlArray = [];
+    $scope.titleArray = [];
+
+    $scope.getReddit = function() {
+        $http({
+            method: 'GET',
+            url: getURL
+        }).then(function(response) {
+            console.log('response', response.data);
+            $scope.a = response.data;
+            $scope.churningHot = $scope.a.data.children;
+
+            angular.forEach($scope.churningHot, function(item) {
+                $scope.titleArray.push(item.data.title);
+                $scope.urlArray.push(item.data.url);
+            });
+        });
+
+    };
 }]);
