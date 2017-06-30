7665d1ce4d30fa6345dffcc36e79484c75f5f518
johnark88 <john.arkema@gmail.com>
2017-06-29 21:17:10 -0700

    medium response error

--- server/routes/api/getRoute.js
+++ server/routes/api/getRoute.js
@@ -1,13 +1,29 @@
-var router = require('express').Router();
-var path = require('path');
-var http = require('http');
+const router = require('express').Router();
+const path = require('path');
+const request = require('request-promise');
 
+var jsonToSend = {};
+const options = {
+    method:'GET',
+    uri:'https://api.medium.com/v1/me',
+    headers: {
+        'Authorization': '2311cc788eed37168ce146ab56e2adf9a8d77eeb870a34005cb3610ec5836de29',
+        
+    },
+    json: true
 
+}
+    request(options).then(function(response){
+        console.log(response.body)
+       var jsonToSend = response;
+        // console.log(jsonToSend, 'JSON TO SEND');
+    })
+    .catch(function (err){
+        console.log(err);
+    })
 
-router.get('/bookmarked', function(req, res){
-    console.log('Get bookmarked articles')
+    
 
-});
 
 
 module.exports = router;
